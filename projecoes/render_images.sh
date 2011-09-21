02d2a2d9d8c032ba72994d39042d739b6a09f49e
Denilson Sá <denilsonsa@gmail.com>
2011-09-21 20:42:06 -0300

    Removed Xephyr support. Just use xvfb-run wrapper script instead.

--- projecoes/render_images.sh
+++ projecoes/render_images.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-# Rendering all images inside Xephyr (or Xnest)
-Xephyr :99 -screen 800x600 &
-XEPHYR_PID=$!
-export DISPLAY=:99
-
-
 # phi and theta offsets
-p=30
+p=0
 t=0
 
 # PHI and THETA apertures
-P=80
-T=80
+#P=80
+#T=80
 
 # White dot size
 DOT_SIZE=2
 
-for abertura in {10..90..10} ; do
+for abertura in {10..90..5} ; do
 	P=${abertura}
 	T=${abertura}
 
 	for a in {1..8} ; do
 		./generate_sphere_vectors.py -P ${P} -T ${T} -p ${p} -t ${t} \
-		| ./convert_coordinates.py -a $a \
-		| ./draw_points.py -p -s ${DOT_SIZE} -q -o "P${P}T${T}p${p}t${t}_a$a.png"
+		| ./convert_coordinates.py -a ${a} \
+		| ./draw_points.py -p -s ${DOT_SIZE} -q -o "P${P}T${T}p${p}t${t}_a${a}.png"
 	done
 done
-
-
-kill "$XEPHYR_PID"
