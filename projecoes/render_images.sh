b73ca10710fbe4d997f988e910ca474940c89a1c
Denilson Sá <denilsonsa@gmail.com>
2011-09-21 19:20:09 -0300

    Added a script to render multiple images.

---
+++ projecoes/render_images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# phi and theta offsets
+p=0
+t=0
+
+# PHI and THETA apertures
+P=80
+T=80
+
+# White dot size
+DOT_SIZE=2
+
+for abertura in {10..90..5} ; do
+	P=${abertura}
+	T=${abertura}
+
+	for a in {1..8} ; do
+		./generate_sphere_vectors.py -P ${P} -T ${T} -p ${p} -t ${t} \
+		| ./convert_coordinates.py -a $a \
+		| ./draw_points.py -p -s ${DOT_SIZE} -q -o "P${P}T${T}p${p}t${t}_a$a.png"
+	done
+done
