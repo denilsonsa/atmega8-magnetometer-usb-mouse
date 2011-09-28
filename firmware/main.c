2ed7290a1dda0f9884936c0e419f4bfc0e106f7f
Denilson Sá <denilsonsa@gmail.com>
2011-09-28 06:15:04 -0300

    Rewrote build_report_from_char() again. This time I saved 174 bytes.

--- firmware/main.c
+++ firmware/main.c
@@ -284,9 +284,6 @@ PROGMEM char usbHidReportDescriptor[USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH] = {
 
 // }}}
 
-// Last char sent
-static uchar last_char = '\0';
-
 // Pointer to RAM for the string being typed.
 static uchar *string_output_pointer = NULL;
 
@@ -300,83 +297,78 @@ static uchar string_output_buffer[80];
 		string_output_pointer = string_output_buffer; \
 	} while(0);
 
-static void build_report_from_char(uchar c) {  // {{{
-	last_char = c;
+typedef struct KeyAndModifier {
+	uchar key;
+	uchar modifier;
+} KeyAndModifier;
 
-	if (c >= '1' && c <= '9') {
-		report_buffer[0] = 0;
-		report_buffer[1] = KEY_1 + c - '1';
-	}
-	else if (c == '0') {
-		report_buffer[0] = 0;
-		report_buffer[1] = KEY_0;
-	}
-	else if (c >= 'a' && c <= 'z') {
-		report_buffer[0] = 0;
-		report_buffer[1] = KEY_A + c - 'a';
-	}
-	else if (c >= 'A' && c <= 'Z') {
+// Warning: ';:' key is c-cedilla in BR-ABNT2 layout
+// Warning: '/?' key is ';:' in BR-ABNT2 layout
+// Other characters not in this lookup table:
+//   \t \n A-Z a-z [\] ^_` {|} ~
+static KeyAndModifier char_to_key[] PROGMEM = {  // {{{
+	{KEY_SPACE    , 0             }, // SPACE
+	{KEY_1        , MOD_SHIFT_LEFT}, // !
+	{0            , 0             }, // "
+	{KEY_3        , MOD_SHIFT_LEFT}, // #
+	{KEY_4        , MOD_SHIFT_LEFT}, // $
+	{KEY_5        , MOD_SHIFT_LEFT}, // %
+	{KEY_7        , MOD_SHIFT_LEFT}, // &
+	{0            , 0             }, // ´
+	{KEY_9        , MOD_SHIFT_LEFT}, // (
+	{KEY_0        , MOD_SHIFT_LEFT}, // )
+	{KEY_8        , MOD_SHIFT_LEFT}, // *
+	{KEY_EQUAL    , MOD_SHIFT_LEFT}, // +
+	{KEY_COMMA    , 0             }, // ,
+	{KEY_MINUS    , 0             }, // -
+	{KEY_PERIOD   , 0             }, // .
+	{0            , 0             }, // /
+	{KEY_0        , 0             }, // 0
+	{KEY_1        , 0             }, // 1
+	{KEY_2        , 0             }, // 2
+	{KEY_3        , 0             }, // 3
+	{KEY_4        , 0             }, // 4
+	{KEY_5        , 0             }, // 5
+	{KEY_6        , 0             }, // 6
+	{KEY_7        , 0             }, // 7
+	{KEY_8        , 0             }, // 8
+	{KEY_9        , 0             }, // 9
+	{KEY_SEMICOLON, MOD_SHIFT_LEFT}, // :
+	{KEY_SEMICOLON, 0             }, // ;
+	{KEY_COMMA    , MOD_SHIFT_LEFT}, // <
+	{KEY_EQUAL    , 0             }, // =
+	{KEY_PERIOD   , MOD_SHIFT_LEFT}, // >
+	{0            , 0             }, // ?
+	{KEY_2        , MOD_SHIFT_LEFT}  // @
+};  // }}}
+
+static void build_report_from_char(uchar c) {  // {{{
+	if (c >= ' ' && c <= '@') {
+		report_buffer[0] = pgm_read_byte_near(&char_to_key[c - ' '].modifier);
+		report_buffer[1] = pgm_read_byte_near(&char_to_key[c - ' '].key);
+	} else if (c >= 'A' && c <= 'Z') {
 		report_buffer[0] = MOD_SHIFT_LEFT;
 		report_buffer[1] = KEY_A + c - 'A';
-	}
-	else {
+	} else if (c >= 'a' && c <= 'z') {
+		report_buffer[0] = 0;
+		report_buffer[1] = KEY_A + c - 'a';
+	} else {
 		switch (c) {
-			case '\n': report_buffer[1] = KEY_ENTER;     break;
-
-			case '\t': report_buffer[1] = KEY_TAB;       break;
-
-			case ' ':  report_buffer[1] = KEY_SPACE;     break;
-
+			case '\n':
+				report_buffer[0] = 0;
+				report_buffer[1] = KEY_ENTER;
+				break;
+			case '\t':
+				report_buffer[0] = 0;
+				report_buffer[1] = KEY_TAB;
+				break;
 			case '_':
-			case '-':  report_buffer[1] = KEY_MINUS;     break;
-
-			case '+':
-			case '=':  report_buffer[1] = KEY_EQUAL;     break;
-
-			// Warning: this key is c-cedilla in BR-ABNT2 layout
-			case ':':
-			case ';':  report_buffer[1] = KEY_SEMICOLON; break;
-
-			case '<':
-			case ',':  report_buffer[1] = KEY_COMMA;     break;
-
-			case '>':
-			case '.':  report_buffer[1] = KEY_PERIOD;    break;
-
-			case '!':  report_buffer[1] = KEY_1;         break;
-			case '@':  report_buffer[1] = KEY_2;         break;
-			case '#':  report_buffer[1] = KEY_3;         break;
-			case '$':  report_buffer[1] = KEY_4;         break;
-			case '%':  report_buffer[1] = KEY_5;         break;
-			case '&':  report_buffer[1] = KEY_7;         break;
-			case '*':  report_buffer[1] = KEY_8;         break;
-			case '(':  report_buffer[1] = KEY_9;         break;
-			case ')':  report_buffer[1] = KEY_0;         break;
+				report_buffer[0] = MOD_SHIFT_LEFT;
+				report_buffer[1] = KEY_MINUS;
+				break;
 
 			default:
 				report_buffer[1] = 0;
-		}
-
-		switch (c) {
-			case '_':
-			case '+':
-			case ':':
-			case '<':
-			case '>':
-
-			case '!':
-			case '@':
-			case '#':
-			case '$':
-			case '%':
-			case '&':
-			case '*':
-			case '(':
-			case ')':
-				report_buffer[0] = MOD_SHIFT_LEFT;
-				break;
-			default:
-				report_buffer[0] = 0;
 		}
 	}
 }  // }}}
@@ -413,7 +405,6 @@ static uchar send_next_char() {  // {{{
 }  // }}}
 
 static void init_keyboard_emulation() {  // {{{
-	last_char = '\0';
 	string_output_pointer = NULL;
 }  // }}}
 
