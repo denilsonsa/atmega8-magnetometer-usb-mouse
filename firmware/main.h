79d7e0d80a499e42c4a087bf2b1b19e1cfafd7d2
Denilson Sá <denilsonsa@gmail.com>
2011-10-21 01:04:14 -0200

    Started writing Report structs

--- firmware/main.h
+++ firmware/main.h
@@ -7,6 +7,30 @@
 #define __main_h_included__
 
 
+typedef struct KeyboardReport {
+	//uchar report_id;
+	uchar modifier;
+	uchar key;
+} KeyboardReport;
+
+typedef struct MouseReport {
+	//uchar report_id;
+	int x;
+	int y;
+	uchar buttons;
+	// TODO: update the report descriptor to this struct
+} MouseReport;
+
+typedef struct HIDReport {
+	// TODO: update the code at main.c and keyemu.c to this struct
+	uchar report_id;
+	union {
+		KeyboardReport keyboard;
+		MouseReport mouse;
+	};
+} HIDReport;
+
+
 #define REPORT_BUFFER_SIZE 6
 extern uchar report_buffer[REPORT_BUFFER_SIZE];
 
