c59f141f1e19f3e4fe31b73e5203c7e18fe04d84
Denilson Sá <denilsonsa@gmail.com>
2011-10-21 05:18:37 -0200

    Okay, I don't need Wheel...

--- firmware/main.c
+++ firmware/main.c
@@ -179,11 +179,10 @@ __attribute__((externally_visible))
 	0x05, 0x01,	   //	 USAGE_PAGE (Generic Desktop)
 	0x09, 0x30,	   //	 USAGE (X)
 	0x09, 0x31,	   //	 USAGE (Y)
-	0x09, 0x38,	   //	 USAGE (Wheel)
 	0x15, 0x81,	   //	 LOGICAL_MINIMUM (-127)
 	0x25, 0x7f,	   //	 LOGICAL_MAXIMUM (127)
 	0x75, 0x08,	   //	 REPORT_SIZE (8)
-	0x95, 0x03,	   //	 REPORT_COUNT (3)
+	0x95, 0x02,	   //	 REPORT_COUNT (2)
 	0x81, 0x06,	   //	 INPUT (Data,Var,Rel)
 
 	0xc0,                          //   END_COLLECTION
