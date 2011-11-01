73f6a7a78539293374036dbfe7beb8f738654085
Denilson Sá <denilsonsa@gmail.com>
2011-11-01 17:17:51 -0200

    Adicionadas imagens de calibração do zero.

--- projection/matplotlib_3d_plot.py
+++ projection/matplotlib_3d_plot.py
@@ -55,6 +55,14 @@ def parse_args():
         help='Draw 3D points (scatter plot)'
     )
     parser.add_argument(
+        '--2d',
+        metavar='PLANE',
+        action='store',
+        choices=['xy', 'xz', 'yz'],
+        dest='twodee',
+        help='Draw 2D graph using one of these planes: xy, xz, yz'
+    )
+    parser.add_argument(
         '-x', '--limit',
         metavar='LIMIT',
         action='store',
@@ -111,11 +119,7 @@ def parse_args():
     return args
 
 
-def main():
-    args = parse_args()
-
-    # Based on "lines3d_demo.py" and "scatter3d_demo.py" from matplotlib
-
+def matplotlib_configure(args):
     # http://stackoverflow.com/questions/3899980/how-to-change-the-font-size-on-a-matplotlib-plot
     # Default font size is 12
     matplotlib.rcParams['font.size'] = args.fontsize
@@ -125,6 +129,40 @@ def main():
 
     fig = pyplot.figure()
     fig.set_size_inches(args.size)
+
+
+def plot_2d(args, input_files):
+    pyplot.grid(True)
+
+    # if twodee is 'xy', d1 will be 'x' and d2 will be 'y'
+    d1 = args.twodee[0]
+    d2 = args.twodee[1]
+
+    #colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    #shapes = ['o', '^', 's', '*']
+
+    for (i,f) in enumerate(input_files):
+        data = load_data(f)
+        if args.points:
+            # s=size, c=color, marker='o'
+            # Size is hardcoded here
+            #pyplot.scatter(
+            #    getattr(data, d1), getattr(data, d2),
+            #    marker=shapes[i%len(shapes)], c=colors[i%len(colors)],
+            #    #s=20, linewidths=0
+            #)
+            pyplot.scatter(getattr(data, d1), getattr(data, d2))
+        elif args.line:
+            pyplot.plot(getattr(data, d1), getattr(data, d2))
+
+    #pyplot.axis('equal')
+
+    pyplot.xlim(-args.limit, args.limit)
+    pyplot.ylim(-args.limit, args.limit)
+
+
+def plot_3d(args, input_files):
+    # Based on "lines3d_demo.py" and "scatter3d_demo.py" from matplotlib
 
     # Reducing the margins
     pyplot.subplots_adjust(left=0, right=1, bottom=0, top=1)
@@ -136,18 +174,17 @@ def main():
     # wspace = 0.2   # the amount of width reserved for blank space between subplots
     # hspace = 0.2   # the amount of height reserved for white space between subplots
 
+    fig = pyplot.gcf()
+
     #ax = fig.gca(projection='3d')
     ax = fig.add_subplot(111, projection='3d')
     ax.grid(True)
-
-    input_files = args.input_files
-    if not args.input_files:
-        input_files = [sys.stdin]
 
     for f in input_files:
         data = load_data(f)
         if args.points:
             # s=size, c=color, marker='o'
+            # Size is hardcoded here
             ax.scatter(data.x, data.y, data.z, s=1, marker='o', linewidths=0)
         elif args.line:
             ax.plot(data.x, data.y, data.z)
@@ -162,6 +199,22 @@ def main():
     # Rotating the view
     #ax.view_init(elev=30, azim=-60)  # default
     #ax.view_init(elev=45, azim=45)
+
+
+def main():
+    args = parse_args()
+
+    input_files = args.input_files
+    if not args.input_files:
+        input_files = [sys.stdin]
+
+
+    matplotlib_configure(args)
+    if args.twodee:
+        plot_2d(args, input_files)
+    else:
+        plot_3d(args, input_files)
+
 
     if args.output:
         for filename in args.output:
